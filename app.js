a009d6eed0f3ed6ef5364fd1e3f015804e1febf2
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-04 21:44:12 +0800

    修正分鐘錯誤

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ setInterval(function () {
     let strdate1 = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate() + " " + dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
     let dt2 = new Date();
     dt2.setTime(dt.getTime() + 1000 * 60);
-    let strdate2 = dt2.getFullYear() + "-" + (dt2.getMonth() + 1) + "-" + dt2.getDate() + " " + dt2.getHours() + ":" + dt.getMinutes() + ":" + dt2.getSeconds();
+    let strdate2 = dt2.getFullYear() + "-" + (dt2.getMonth() + 1) + "-" + dt2.getDate() + " " + dt2.getHours() + ":" + dt2.getMinutes() + ":" + dt2.getSeconds();
     // let sql = "SELECT * FROM `pofeed` WHERE `po_time`='"+strdate1+"'";
     let sql = "UPDATE `pofeed` SET `content`='success' WHERE `po_time`>='" + strdate1 + "' AND `po_time`<='" + strdate2 + "'";
     console.log(sql);
