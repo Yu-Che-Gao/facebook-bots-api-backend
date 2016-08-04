5429b674f76f1ba4b0f174cc066a544045c6422d
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-05 00:17:31 +0800

    更新sql指令

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var j = schedule.scheduleJob(rule, function () {
     dt2.setTime(dt.getTime() - 1000 * 60);
     let strdate2 = dt2.getFullYear() + "-" + (dt2.getMonth() + 1) + "-" + dt2.getDate() + " " + dt2.getHours() + ":" + dt2.getMinutes() + ":" + dt2.getSeconds();
     // let sql = "SELECT * FROM `pofeed` WHERE `po_time`='"+strdate1+"'";
-    let sql = "UPDATE `pofeed` SET `content`='success' WHERE `po_time`<='" + strdate1 + "' AND `po_time`>='" + strdate2 + "'";
+    let sql = "UPDATE `pofeed` SET `content`='success' WHERE `po_time`<='" + strdate1 + "'";
     console.log(sql);
     mysql.getUpdate(sql);
 });
