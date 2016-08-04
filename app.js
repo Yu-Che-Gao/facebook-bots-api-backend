515d315bb5e7180c997b9a3c7f06370ec23d9f52
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-04 21:48:24 +0800

    修正時間條件

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ setInterval(function () {
     let dt = timeZone.getTimeZone(8);
     let strdate1 = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate() + " " + dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
     let dt2 = new Date();
-    dt2.setTime(dt.getTime() + 1000 * 60);
+    dt2.setTime(dt.getTime() - 1000 * 60);
     let strdate2 = dt2.getFullYear() + "-" + (dt2.getMonth() + 1) + "-" + dt2.getDate() + " " + dt2.getHours() + ":" + dt2.getMinutes() + ":" + dt2.getSeconds();
     // let sql = "SELECT * FROM `pofeed` WHERE `po_time`='"+strdate1+"'";
-    let sql = "UPDATE `pofeed` SET `content`='success' WHERE `po_time`>='" + strdate1 + "' AND `po_time`<='" + strdate2 + "'";
+    let sql = "UPDATE `pofeed` SET `content`='success' WHERE `po_time`<='" + strdate1 + "' AND `po_time`>='" + strdate2 + "'";
     console.log(sql);
-    mysql.getUpdate(sql);
-}, 1000 * 60);
+    // mysql.getUpdate(sql);
+}, 1000);
