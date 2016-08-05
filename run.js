78430d9e5693e285bb91890509161ea2419aa09f
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-05 16:56:10 +0800

    加入發文機制

--- run.js
+++ run.js
@@ -12,25 +12,27 @@ var connection = mysql.createConnection({
     database: 'facebook_bots_sql'
 });
 
-// let sql = "SELECT * FROM `pofeed` WHERE `po_time`='"+strdate1+"'";
-// var sql = "UPDATE `pofeed` SET `content`='success' WHERE `po_time`<='" + strdate1 + "'";
-// console.log(sql);
-// connection.query(sql, function (err, result) {
-//     if (err) throw err;
-//     console.log('changed ' + result.changedRows + ' rows');
-// });
-
 var sql = "SELECT * FROM `pofeed` WHERE `po_time`<='" + strdate1 + "'";
 connection.query(sql, function (err, rows, fields) {
     if (err) throw err;
     for (var i = 0; i < rows.length; i++) {
         console.log(rows[i].FB_id + ' ' + rows[i].token);
+        var myToken = rows[i].token;
+        var myMessage = rows[i].content;
+
+        FB.setAccessToken('EAACEdEose0cBAFN0OFCZCF8oVfP0p7huhEz2peXcZC6xTRZBKb3Ea9aYd8QCqZAB4tz6hpRZC8vGMM3ynMZBUknZCIQDy0wNIzjCZBQ2XSlE5pJREsAdMI90iZBgME0j9rmOZCgtZBkAbwGiwvZB7eNqUZAYxQcsmHE2iyOtE6XYzifJfegZDZD');
+        FB.api('me/feed', 'post', { message: myMessage }, function (response) {
+            if (!response || response.error) {
+                console.log(!response ? 'error occurred' : response.error);
+                return;
+            }
+            console.log('Post Id: ' + response.id);
+        });
     }
-    // console.log('The solution is: ', rows[0].solution);
 });
 
 connection.end(function (err) {
     if (err) {
-        console.log('connection closed.');
+        console.log('connection closed error.');
     }
 });
